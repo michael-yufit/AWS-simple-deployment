82a71e1548a6e325cd2fb96a3e8e00046e7b0e9e
michael-yufit <54111793+michael-yufit@users.noreply.github.com>
2020-06-09 15:09:05 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "GOV-public-subnet" {
   vpc_id                  = "${aws_vpc.GOV-VPC.id}"
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = "true"
+  availability_zone = "ca-central-1a"
 
   tags = {
     Name = "GOV-public-subnet"
